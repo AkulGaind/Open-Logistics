2c9440f20d72f4bd719d389037098eafdd571e1d
Muskan Vaish <vaishmuskan531@gmail.com>
2024-05-26 22:39:19 +0530

    Add scrolling to top

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,20 @@ const App = () => {
   );
   const location = useLocation();
   useEffect(() => {
-    return () => {
-      document.body.style.overflow = "auto";
+    // Scroll to the top when the component mounts
+    window.scrollTo(0, 0);
+
+    // Scroll to the top before unloading the page (refreshing)
+    const onBeforeUnload = () => {
       window.scrollTo(0, 0);
+    };
+
+    // Attach the event listener
+    window.onbeforeunload = onBeforeUnload;
+
+    // Clean up by removing the event listener when the component unmounts
+    return () => {
+      window.onbeforeunload = null;
     };
   }, [location.pathname]);
   const noFooterPaths = ["/login", "/signup"];
