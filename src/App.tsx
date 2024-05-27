bf4c618ff2e9a9b73f2261bfc23f9cffb460505e
AkulGaind <akulgaind@gmail.com>
2024-05-27 16:07:45 +0530

    Delete comments

--- src/App.tsx
+++ src/App.tsx
@@ -19,18 +19,11 @@ const App = () => {
   );
   const location = useLocation();
   useEffect(() => {
-    // Scroll to the top when the component mounts
     window.scrollTo(0, 0);
-
-    // Scroll to the top before unloading the page (refreshing)
     const onBeforeUnload = () => {
       window.scrollTo(0, 0);
     };
-
-    // Attach the event listener
     window.onbeforeunload = onBeforeUnload;
-
-    // Clean up by removing the event listener when the component unmounts
     return () => {
       window.onbeforeunload = null;
     };
